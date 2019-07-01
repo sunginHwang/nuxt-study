cd8541d1cf80baceadf1cd4754b37f40aab76907
hwangseong-in <gommpo111@gmail.com>
2019-07-01 18:52:54 +0900

    asyncData 를 통한 ssr 에서 fetch 후 랜더링 대상 부를 수 있는 구성 작업. asyncData 에서 return 한 값이 data 에 바인딩 된다.

---
+++ pages/posts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>타이틀</h1>
+    <nuxt-link to="/postsSSR">go posts SSR</nuxt-link>
+
+    <ul v-for="(post, index) in posts" :key="index">
+      <li>{{post.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+    export default {
+        name: "posts",
+      data() {
+        return {
+          posts: []
+        }
+      },
+      async created() {
+        const apiRes = await axios.get('https://api-blog.woolta.com/post/categories/new/posts');
+        return {posts: apiRes.data.data};
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/postsSSR.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>타이틀</h1>
+    <nuxt-link to="/posts">go posts</nuxt-link>
+    <ul v-for="(post, index) in posts" :key="index">
+      <li>{{post.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: "postsSSR",
+    data() {
+      return {
+        posts: []
+      }
+    },
+    async asyncData ({ req, res }) {
+      console.log('asyncData');
+      const apiRes = await axios.get('https://api-blog.woolta.com/post/categories/new/posts');
+      return {posts : apiRes.data.data};
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
