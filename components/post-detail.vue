b47385dbca8052051d428aa4e2da0d935218e767
hwangseong-in <gommpo111@gmail.com>
2019-07-02 18:14:10 +0900

    생명 주기 블로깅용 작업 중

---
+++ components/post-detail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{content}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "post-detail",
+    watch: {
+      content() {
+        console.log('watch content');
+      },
+    },
+    props: {
+      content: {
+        type: String,
+        default: ''
+      },
+      created() {
+        console.log('post-detail');
+      },
+      updated() {
+        console.log('post-detail updated');
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/page/_id/post.vue
+++ pages/page/_id/post.vue
@@ -1,17 +1,43 @@
 <template>
-  <p>post {{this.$route.params.id}}</p>
+ <div>
+   <p>post {{this.$route.params.id}}</p>
+   <nuxt-link :to="goPostUrl">go post {{updateCount}}</nuxt-link>
+     <postDetail :content="isDevideZero ? `짝수` : `홀수`"/>
+ </div>
 </template>
 
 <script>
+  import postDetail from '../../../components/post-detail';
   export default {
     name: "post",
+    components:{
+      postDetail
+    },
     validate({params}) {
       // Must be a number
       return /^\d+$/.test(params.id)
     },
     created() {
-      console.log(this.$route.params.id);
-    }
+      console.log(`post create ${this.$route.params.id}`);
+    },
+    updated() {
+      console.log(`post update. count:${this.updateCount}`)
+    },
+    computed: {
+      goPostUrl() {
+        return `/page/${Number(this.$route.params.id )+ 1}/post`;
+      },
+      isDevideZero(){
+        console.log(Number(this.$route.params.id));
+        console.log(Number(this.$route.params.id) % 2);
+        return Number(this.$route.params.id) % 2 === 0;
+      }
+    },
+    data() {
+      return {
+        updateCount: 1
+      }
+    },
   }
 </script>
 
