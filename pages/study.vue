bd5de7819f234a21206a39e0e7058f3775395c12
hwangseong-in <gommpo111@gmail.com>
2019-07-11 22:58:01 +0900

    로컬 이미지 바인딩 사용 스터디

--- pages/study.vue
+++ pages/study.vue
@@ -1,13 +1,24 @@
 <template>
+  <div>
     <div>
       <p>study</p>
     </div>
+    <div>
+      <h2>이미지 바인딩 테스트</h2>
+      <img :src="testSrc">
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "study"
-    }
+  export default {
+    name: "study",
+    computed: {
+      testSrc() {
+        return require('../assets/image/btn-my.svg');
+      }
+    },
+  }
 </script>
 
 <style scoped>
